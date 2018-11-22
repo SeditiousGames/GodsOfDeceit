e90ff278b399c8602fa03d38e3af81e4450e9fc0
Mohammad S. Babaei <info@babaei.net>
2018-11-22 20:25:02 +0100

    set real values for log channels since the blocking bug for this feature has been fixed in UE 4.21

--- Source/GodsOfDeceitLog/Public/GLog/GLog.h
+++ Source/GodsOfDeceitLog/Public/GLog/GLog.h
@@ -63,15 +63,15 @@ DECLARE_LOG_CATEGORY_EXTERN ( Log_SQL, All, All );
 DECLARE_LOG_CATEGORY_EXTERN ( Log_Weapon, All, All );
 
 static constexpr uint64 GLOG_KEY_INFINITE = static_cast<uint64>(-1);
-static constexpr uint64 GLOG_KEY_AI = static_cast<uint64>(-1);  /* For the time being, due to a bug in 4.19+ any value other than -1 won't work */
-static constexpr uint64 GLOG_KEY_ANIMATION = static_cast<uint64>(-1);  /* For the time being, due to a bug in 4.19+ any value other than -1 won't work */
-static constexpr uint64 GLOG_KEY_BLUEPRINTS = static_cast<uint64>(-1);  /* For the time being, due to a bug in 4.19+ any value other than -1 won't work */
-static constexpr uint64 GLOG_KEY_EDITOR = static_cast<uint64>(-1);  /* For the time being, due to a bug in 4.19+ any value other than -1 won't work */
-static constexpr uint64 GLOG_KEY_GENERIC = static_cast<uint64>(-1);  /* For the time being, due to a bug in 4.19+ any value other than -1 won't work */
-static constexpr uint64 GLOG_KEY_INPUT = static_cast<uint64>(-1);  /* For the time being, due to a bug in 4.19+ any value other than -1 won't work */
-static constexpr uint64 GLOG_KEY_PLAYER = static_cast<uint64>(-1);  /* For the time being, due to a bug in 4.19+ any value other than -1 won't work */
-static constexpr uint64 GLOG_KEY_SQL = static_cast<uint64>(-1);  /* For the time being, due to a bug in 4.19+ any value other than -1 won't work */
-static constexpr uint64 GLOG_KEY_WEAPON = static_cast<uint64>(-1);  /* For the time being, due to a bug in 4.19+ any value other than -1 won't work */
+static constexpr uint64 GLOG_KEY_AI = static_cast<uint64>(800);
+static constexpr uint64 GLOG_KEY_ANIMATION = static_cast<uint64>(500);
+static constexpr uint64 GLOG_KEY_BLUEPRINTS = static_cast<uint64>(600);
+static constexpr uint64 GLOG_KEY_EDITOR = static_cast<uint64>(300);
+static constexpr uint64 GLOG_KEY_GENERIC = static_cast<uint64>(100);
+static constexpr uint64 GLOG_KEY_INPUT = static_cast<uint64>(400);
+static constexpr uint64 GLOG_KEY_PLAYER = static_cast<uint64>(700);
+static constexpr uint64 GLOG_KEY_SQL = static_cast<uint64>(200);
+static constexpr uint64 GLOG_KEY_WEAPON = static_cast<uint64>(600);
 
 template<typename TYPE>
 struct GLogString
