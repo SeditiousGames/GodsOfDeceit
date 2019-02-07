eb4f7a8e1c0118c6e10f50a143ae250afe2dedc4
Mohammad S. Babaei <info@babaei.net>
2019-02-07 15:51:00 +0100

    partial implementation for the health component

---
+++ Source/GodsOfDeceit/Private/GGameFramework/GHealthComponent.cpp
@@ -0,0 +1,195 @@
+/**
+ * @file
+ * @author  Mamadou Babaei <info@babaei.net>
+ * @version 0.1.0
+ *
+ * @section LICENSE
+ *
+ * (The MIT License)
+ *
+ * Copyright (c) 2018 - 2019 Mamadou Babaei
+ * Copyright (c) 2018 - 2019 Seditious Games Studio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @section DESCRIPTION
+ *
+ * A health component which manages damage and healing.
+ */
+
+
+#include "GGameFramework/GHealthComponent.h"
+#include "GodsOfDeceit.h"
+
+#include <utility>
+
+#include <Async/Async.h>
+#include <Engine/EngineTypes.h>
+#include <GameFramework/Actor.h>
+
+struct UGHealthComponent::Impl
+{
+public:
+    FTimerHandle HealthRegenerationTimer;
+    bool bPlummetingStarted;
+
+private:
+    UGHealthComponent* Owner;
+
+public:
+    explicit Impl(UGHealthComponent* InOwner);
+    ~Impl();
+
+public:
+    void UpdateFallingDamage(const FVector& DeltaLocation);
+};
+
+UGHealthComponent::UGHealthComponent(
+        const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    this->PrimaryComponentTick.bCanEverTick = true;
+
+    this->SetCanEverAffectNavigation(false);
+
+    this->Health = 100.0f;
+    this->bHealthRegeneration = false;
+
+    AsyncTask(ENamedThreads::GameThread, [&]()
+    {
+        this->HealthRegenerationDelay.Initialize(this);
+        this->HealthRegenerationDelay.Recruit = 5.0f;
+        this->HealthRegenerationDelay.Regular = 5.0f;
+        this->HealthRegenerationDelay.Hardened = 5.0f;
+        this->HealthRegenerationDelay.Veteran = 5.0f;
+
+        this->HealthRegenerationInterval.Initialize(this);
+        this->HealthRegenerationInterval.Recruit = 0.1f;
+        this->HealthRegenerationInterval.Regular = 0.1f;
+        this->HealthRegenerationInterval.Hardened = 0.1f;
+        this->HealthRegenerationInterval.Veteran = 0.1f;
+
+        this->HealthRegenerationPerSecond.Initialize(this);
+        this->HealthRegenerationPerSecond.Recruit = 5.0f;
+        this->HealthRegenerationPerSecond.Regular = 5.0f;
+        this->HealthRegenerationPerSecond.Hardened = 5.0f;
+        this->HealthRegenerationPerSecond.Veteran = 5.0f;
+
+        this->SafeFallingDistance.Initialize(this);
+        this->SafeFallingDistance.Recruit = 300.0f;
+        this->SafeFallingDistance.Regular = 300.0f;
+        this->SafeFallingDistance.Hardened = 300.0f;
+        this->SafeFallingDistance.Veteran = 300.0f;
+
+        this->FallingDamagePerMeter.Initialize(this);
+        this->FallingDamagePerMeter.Recruit = 30.0f;
+        this->FallingDamagePerMeter.Regular = 30.0f;
+        this->FallingDamagePerMeter.Hardened = 30.0f;
+        this->FallingDamagePerMeter.Veteran = 30.0f;
+    });
+}
+
+void UGHealthComponent::OnTakeAnyDamage(
+        AActor* DamagedActor, float Damage, const UDamageType* DamageType,
+        AController* InstigatedBy, AActor* DamageCauser)
+{
+
+}
+
+void UGHealthComponent::OnFallingBegin(const FVector& StartLocation)
+{
+
+}
+
+void UGHealthComponent::OnFallingUpdate(const FVector& StartLocation,
+                                        const FVector& LocationDelta)
+{
+
+}
+
+void UGHealthComponent::OnFallingEnd(const FVector& StartLocation,
+                                     const FVector& EndLocation,
+                                     const FVector& LocationDelta)
+{
+
+}
+
+void UGHealthComponent::OnHealthRegenerationTick()
+{
+
+}
+
+
+void UGHealthComponent::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void UGHealthComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+    Super::EndPlay(EndPlayReason);
+}
+
+void UGHealthComponent::TickComponent(
+        float DeltaTime, ELevelTick TickType,
+        FActorComponentTickFunction *ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UGHealthComponent::SetHealth(const float Amount,
+                                  const UDamageType* DamageType,
+                                  AController* InstigatedBy,
+                                  AActor* DamageCauser)
+{
+
+}
+
+void UGHealthComponent::Damage(const float Amount,
+                               const UDamageType* DamageType,
+                               AController* InstigatedBy,
+                               AActor* DamageCauser)
+{
+
+}
+
+void UGHealthComponent::Heal(const float Amount)
+{
+
+}
+
+UGHealthComponent::Impl::Impl(UGHealthComponent* InOwner)
+    : bPlummetingStarted(false),
+      Owner(InOwner)
+{
+
+}
+
+UGHealthComponent::Impl::~Impl() = default;
+
+void UGHealthComponent::Impl::UpdateFallingDamage(const FVector& DeltaLocation)
+{
+
+}
+
+void UGHealthComponent::ImplDeleter::operator()(
+        UGHealthComponent::Impl* Pointer)
+{
+    delete Pointer;
+}
---
+++ Source/GodsOfDeceit/Private/GGameFramework/GHealthComponent.h
@@ -0,0 +1,157 @@
+/**
+ * @file
+ * @author  Mamadou Babaei <info@babaei.net>
+ * @version 0.1.0
+ *
+ * @section LICENSE
+ *
+ * (The MIT License)
+ *
+ * Copyright (c) 2018 - 2019 Mamadou Babaei
+ * Copyright (c) 2018 - 2019 Seditious Games Studio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @section DESCRIPTION
+ *
+ * A health component which manages damage and healing.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include <Components/ActorComponent.h>
+#include <CoreTypes.h>
+#include <Math/Vector.h>
+#include <UObject/ObjectMacros.h>
+
+#include "GDifficultyLevel/GDLV_float.h"
+
+#include "GHealthComponent.generated.h"
+
+UCLASS(ClassGroup=(GODSOFDECEIT), meta=(BlueprintSpawnableComponent))
+class GODSOFDECEIT_API UGHealthComponent : public UActorComponent
+{
+    GENERATED_UCLASS_BODY()
+
+private:
+    struct Impl;
+    struct ImplDeleter
+    {
+      void operator()(Impl* Pointer);
+    };
+    std::unique_ptr<Impl, ImplDeleter> Pimpl;
+    ImplDeleter PimplDeleter;
+
+public:
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE_SixParams(FOnHealthLevelChangedSignature, UGHealthComponent*, OwningHealthComponent, float, Health, float, HealthDelta, const UDamageType*, DamageType, AController*, InstigatedBy, AActor*, DamageCauser);
+
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlummetingBeginSignature, const float, Health);
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlummetingUpdateSignature, const float, Health);
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlummetingEndSignature, const float, Health);
+
+public:
+    FOnHealthLevelChangedSignature OnHealthLevelChanged;
+
+    FOnPlummetingBeginSignature OnPlummetingBegin;
+    FOnPlummetingUpdateSignature OnPlummetingUpdate;
+    FOnPlummetingEndSignature OnPlummetingEnd;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Health Component")
+    float Health;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Health Component")
+    uint8 bHealthRegeneration : 1;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Health Component")
+    FGDLV_float HealthRegenerationDelay;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Health Component")
+    FGDLV_float HealthRegenerationInterval;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Health Component")
+    FGDLV_float HealthRegenerationPerSecond;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Health Component")
+    FGDLV_float SafeFallingDistance;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Health Component")
+    FGDLV_float FallingDamagePerMeter;
+
+protected:
+    UFUNCTION (  )
+    virtual void OnTakeAnyDamage(
+            AActor* DamagedActor, float Damage, const UDamageType* DamageType,
+            AController* InstigatedBy, AActor* DamageCauser);
+
+    UFUNCTION (  )
+    virtual void OnFallingBegin(const FVector& StartLocation);
+
+    UFUNCTION (  )
+    virtual void OnFallingUpdate(const FVector& StartLocation,
+                                 const FVector& LocationDelta);
+
+    UFUNCTION (  )
+    virtual void OnFallingEnd(const FVector& StartLocation,
+                              const FVector& EndLocation,
+                              const FVector& LocationDelta);
+
+    void OnHealthRegenerationTick();
+
+    virtual void BeginPlay() override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+    virtual void TickComponent(
+            float DeltaTime, ELevelTick TickType,
+            FActorComponentTickFunction *ThisTickFunction) override;
+
+public:
+    FORCEINLINE float GetMaxHealth() const
+    {
+        return GetClass()->GetDefaultObject<UGHealthComponent>()->Health;
+    }
+
+    FORCEINLINE float GetHealth() const
+    {
+        return Health;
+    }
+
+    void SetHealth(const float Amount,
+                   const UDamageType* DamageType = nullptr,
+                   AController* InstigatedBy = nullptr,
+                   AActor* DamageCauser = nullptr);
+
+    void Damage(const float Amount,
+                const UDamageType* DamageType,
+                AController* InstigatedBy,
+                AActor* DamageCauser);
+    void Heal(const float Amount);
+
+    FORCEINLINE bool IsAlive() const
+    {
+        return Health > 0.0f;
+    }
+
+    FORCEINLINE bool IsDead() const
+    {
+        return Health <= 0.0f;
+    }
+};
