6d2428a8b999035a0363f71941f676d4b88537b5
Mamadou Babaei <info@babaei.net>
2019-05-04 16:01:24 +0200

    fix blob cleaner script

--- Scripts/blob-cleaner.sh
+++ Scripts/blob-cleaner.sh
@@ -26,15 +26,16 @@
 
 SCRIPTS_DIRECTORY=$(dirname $(realpath "$0"))
 PROJECT_DIRECTORY=`dirname $(dirname $(realpath "$0"))`
-REMOTE_REPOSITORY="git@gitlab.com:Seditious Games Studio/GodsOfDeceit_Content_ThirdParty.git"
-BACKUP_REPOSITORY="/home/babaei/dev/GodsOfDeceit-bare-repo.git"
-BACKUP_DIRECTORY=$(dirname "${BACKUP_REPOSITORY}")
+THIRDPARTY_REMOTE_REPOSITORY="git@gitlab.com:Seditious Games Studio/GodsOfDeceit_Content_ThirdParty.git"
+THIRDPARTY_LOCAL_DIRECTORY="${PROJECT_DIRECTORY}/ThirdParty"
+THIRDPARTY_BACKUP_REPOSITORY="/home/mamadou/dev/GodsOfDeceit-bare-repo.git"
+THIRDPARTY_BACKUP_DIRECTORY=$(dirname "${THIRDPARTY_BACKUP_REPOSITORY}")
 
 wget -q --no-check-certificate http://repo1.maven.org/maven2/com/madgag/bfg/1.13.0/bfg-1.13.0.jar -O ${SCRIPTS_DIRECTORY}/bfg.jar
 
-mkdir -p ${BACKUP_DIRECTORY} \
-    && git clone --mirror ${REMOTE_REPOSITORY} ${BACKUP_REPOSITORY} \
-    && java -jar ${SCRIPTS_DIRECTORY}/bfg.jar --strip-blobs-bigger-than 1M --protect-blobs-from master ${PROJECT_DIRECTORY} \
-    && cd ${PROJECT_DIRECTORY} \
+mkdir -p ${THIRDPARTY_BACKUP_DIRECTORY} \
+    && git clone --mirror ${THIRDPARTY_REMOTE_REPOSITORY} ${THIRDPARTY_BACKUP_REPOSITORY} \
+    && java -jar ${SCRIPTS_DIRECTORY}/bfg.jar --strip-blobs-bigger-than 1M --protect-blobs-from master ${THIRDPARTY_LOCAL_DIRECTORY} \
+    && cd ${THIRDPARTY_LOCAL_DIRECTORY} \
     && git reflog expire --expire=now --all && git gc --prune=now --aggressive \
     && git push --force --all
